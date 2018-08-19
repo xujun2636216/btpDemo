73b8fc7908f28a4250524231f9ff8b3b6c555c06
btpdemo <xujun6868168@163.com>
2018-08-19 23:05:52 +0800

    mybait的使用

---
+++ Multithreading/ThreadOperation/ForkJoin.java
@@ -0,0 +1,13 @@
+package ThreadOperation;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoin {
+
+    public static void main(final String[] arguments) throws InterruptedException, ExecutionException {
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+    }
+}
---
+++ Multithreading/ThreadOperation/FuturesandCallables.java
@@ -0,0 +1,49 @@
+package ThreadOperation;
+
+import java.util.concurrent.*;
+
+public class FuturesandCallables {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        System.out.println("Factorial Service called for 10!");
+        Future<Long> result10 = executor.submit(new FactorialService(10));
+
+        System.out.println("Factorial Service called for 20!");
+        Future<Long> result20 = executor.submit(new FactorialService(20));
+
+        Long factorial10 = result10.get();
+        System.out.println("10! = " + factorial10);
+
+        Long factorial20 = result20.get();
+        System.out.println("20! = " + factorial20);
+
+        executor.shutdown();
+    }
+
+    public static class FactorialService implements Callable<Long> {
+        private int number;
+
+        public FactorialService(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public Long call() throws Exception {
+            return factorial();
+        }
+
+        private Long factorial() throws InterruptedException {
+            long result = 1;
+            while (number != 0) {
+                result = number * result;
+                number--;
+                Thread.sleep(100);
+            }
+            return result;
+        }
+    }
+
+}
