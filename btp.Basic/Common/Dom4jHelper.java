57819597f3d8c66bfbcf47a111124a28734d1f98
btpdemo <xujun6868168@163.com>
2018-08-25 12:15:47 +0800

    mybait的使用

--- btp.Basic/Common/Dom4jHelper.java
+++ btp.Basic/Common/Dom4jHelper.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package Common;
 
@@ -13,6 +13,7 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
+
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -29,62 +30,62 @@ import org.dom4j.io.XMLWriter;
  */
 public class Dom4jHelper {
 	/**
-	 *  ����url xml�ĵ�
+	 *  解析url xml文档
 	 * @param url
 	 * @return
 	 * @throws DocumentException
 	 */
-    public Document parse(URL url) throws DocumentException {
-        SAXReader reader = new SAXReader();
-        Document document = reader.read(url);
-        return document;
-    }
-    /**
-     * ���������ĵ�
-     * @param document
-     */
-    public void treeWalk(Document document) {
-        treeWalk( document.getRootElement() );
-    }
-    /**
-     * ��������Ԫ��
-     * @param element
-     */
-    public void treeWalk(Element element) {
-        for ( int i = 0, size = element.nodeCount(); i < size; i++ ) {
-            Node node = element.node(i);
-            if ( node instanceof Element ) {
-                treeWalk( (Element) node );
-            }
-            else {
-                // ����....
-            }
-        }
-    }
+	public Document parse(URL url) throws DocumentException {
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(url);
+		return document;
+	}
+	/**
+	 * 遍历解析文档
+	 * @param document
+	 */
+	public void treeWalk(Document document) {
+		treeWalk( document.getRootElement() );
+	}
+	/**
+	 * 遍历解析元素
+	 * @param element
+	 */
+	public void treeWalk(Element element) {
+		for ( int i = 0, size = element.nodeCount(); i < size; i++ ) {
+			Node node = element.node(i);
+			if ( node instanceof Element ) {
+				treeWalk( (Element) node );
+			}
+			else {
+				// 处理....
+			}
+		}
+	}
 
-	/** 
-	 * �����ļ�����ø�Ԫ��
+	/**
+	 * 解析文件，获得根元素
 	 * @param xmlPath
 	 * @param encoding
 	 * @return
 	 * @throws Exception
 	 */
 	public static Element parse(String xmlPath,String encoding)throws Exception{
-		//�ļ��Ƿ����
+		//文件是否存在
 		File file = new File(xmlPath);
-        if(!file.exists()){
-        	throw new Exception("�Ҳ���xml�ļ���"+xmlPath);
-        }
-        
-		//����
+		if(!file.exists()){
+			throw new Exception("找不到xml文件："+xmlPath);
+		}
+
+		//解析
 		SAXReader reader = new SAXReader(false);
 		Document doc = reader.read(new FileInputStream(file),encoding);
 		Element root = doc.getRootElement();
 		return root;
 	}
-	
+
 	/**
-	 * �����ĵ�
+	 * 保存文档
 	 * @param doc
 	 * @param xmlPath
 	 * @param encoding
@@ -92,19 +93,19 @@ public class Dom4jHelper {
 	 */
 	public static void save(Document doc,String xmlPath,String encoding)throws Exception{
 		OutputFormat format=OutputFormat.createPrettyPrint();
-	    format.setEncoding(encoding);
-	    XMLWriter writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(xmlPath),encoding),format);
+		format.setEncoding(encoding);
+		XMLWriter writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(xmlPath),encoding),format);
 		writer.write(doc);
 		writer.flush();
-		writer.close();	
+		writer.close();
 	}
 	/**
-	 * �޸�xmlĳ�ڵ��ֵ
-	 * @param inputXml ԭxml�ļ�
-	 * @param nodes Ҫ�޸ĵĽڵ�
-	 * @param attributename ��������
-	 * @param value ��ֵ
-	 * @param outXml ����ļ�·�����ļ��� �������ļ�Ϊnull����Ĭ��Ϊԭxml�ļ�
+	 * 修改xml某节点的值
+	 * @param inputXml 原xml文件
+	 * @param nodes 要修改的节点
+	 * @param attributename 属性名称
+	 * @param value 新值
+	 * @param outXml 输出文件路径及文件名 如果输出文件为null，则默认为原xml文件
 	 */
 	public static void modifyDocument(File inputXml, String nodes, String attributename, String value, String outXml) {
 		try {
@@ -118,9 +119,9 @@ public class Dom4jHelper {
 					attribute.setValue(value);
 			}
 			XMLWriter output;
-			if (outXml != null){ //ָ������ļ�
+			if (outXml != null){ //指定输出文件
 				output = new XMLWriter(new FileWriter(new File(outXml)));
-			}else{ //����ļ�Ϊԭ�ļ�
+			}else{ //输出文件为原文件
 				output = new XMLWriter(new FileWriter(inputXml));
 			}
 			output.write(document);
@@ -132,10 +133,10 @@ public class Dom4jHelper {
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-	}	
-	
+	}
+
 	/**
-	 * xmlת��Ϊ�ַ���
+	 * xml转换为字符串
 	 * @param doc
 	 * @param encoding
 	 * @return
@@ -143,32 +144,26 @@ public class Dom4jHelper {
 	 */
 	public static String toString(Document doc,String encoding)throws Exception{
 		OutputFormat format=OutputFormat.createPrettyPrint();
-	    format.setEncoding(encoding);
-	    ByteArrayOutputStream byteOS=new ByteArrayOutputStream();
-	    XMLWriter writer = new XMLWriter(new OutputStreamWriter(byteOS,encoding),format);
+		format.setEncoding(encoding);
+		ByteArrayOutputStream byteOS=new ByteArrayOutputStream();
+		XMLWriter writer = new XMLWriter(new OutputStreamWriter(byteOS,encoding),format);
 		writer.write(doc);
 		writer.flush();
-		writer.close();		
+		writer.close();
 		writer=null;
-		
+
 		return byteOS.toString(encoding);
 	}
 	/**
-	 * �ַ���ת��ΪDocument
+	 * 字符串转换为Document
 	 * @param text
 	 * @return
 	 * @throws DocumentException
 	 */
 	public static Document str2Document(String text) throws DocumentException{
 		Document document = DocumentHelper.parseText(text);
-        return document;
+		return document;
 	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
