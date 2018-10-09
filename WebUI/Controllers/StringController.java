eeea6e56307a96a8a5e2f262dd0760de81ae585a
军 徐 <xujun2636216@hotmail.com>
2018-10-09 19:08:17 +0800

    Dom4j的使用

--- WebUI/Controllers/StringController.java
+++ WebUI/Controllers/StringController.java
@@ -6,8 +6,6 @@ public class StringController {
      * @param args
      */
     public static void main(String[] args) {
-        int a=10;
-        String str="10";
-        str= String.valueOf(a);
+
     }
 }
